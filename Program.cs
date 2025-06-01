01e324551127750411e2054810472bb113cd837e
thiagoMilitao <thiago.militao2007@gmail.com>
2025-06-01 00:01:20 -0300

    RetContorno

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Digite a altura do retângulo (entre 1 e 10):");
+int altura = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite a largura do retângulo (entre 1 e 10):");
+int largura = int.Parse(Console.ReadLine());
+
+if (altura < 1 || altura > 10 || largura < 1 || largura > 10)
+{
+    Console.WriteLine("Altura e largura devem estar entre 1 e 10.");
+    return;
+}
+
+for (int i = 0; i < largura; i++)
+{
+    Console.Write("*");
+}
+Console.WriteLine();
+
+for (int i = 0; i < altura - 2; i++)
+{
+    Console.Write("*");
+    for (int j = 0; j < largura - 2; j++)
+    {
+        Console.Write(" ");
+    }
+    if (largura > 1)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < largura; i++)
+{
+    Console.Write("*");
+
+    }
